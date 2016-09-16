29aeec0a1f2f50974c2b2efa48c3e3a4cc43e0fb
Lester Mesa <lesterm@gmail.com>
2016-09-16 02:46:26 +0000

    Users migration, model, seeder, factory

--- database/migrations/2016_09_15_110048_create_providers_table.php
+++ database/migrations/2016_09_15_110048_create_providers_table.php
@@ -15,6 +15,21 @@ class CreateProvidersTable extends Migration
     {
         Schema::create('providers', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('type')->nullable();
+            $table->string('name')->unique();
+            $table->string('address_1')->nullable();
+            $table->string('address_2')->nullable();
+            $table->string('city')->nullable();
+            $table->char('state', 2)->nullable();
+            $table->string('zip')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('fax')->nullable();
+            $table->string('website')->nullable();
+            $table->string('external_account')->nullable();
+            $table->string('contact')->nullable();
+            $table->string('contact_phone')->nullable();
+            $table->text('notes')->nullable();
+            $table->integer('active')->default(1);
             $table->timestamps();
         });
     }
