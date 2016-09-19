c03c3ab4b9287b8ea165ea5402ce2a1f62d0c97f
Lester Mesa <lesterm@gmail.com>
2016-09-19 18:11:43 +0000

    display company info and datatables for company clients

--- resources/views/admin/company/show.blade.php
+++ resources/views/admin/company/show.blade.php
@@ -25,7 +25,7 @@
             <!-- Profile Image -->
             <div class="box box-primary">
                 <div class="box-body box-profile">
-                    <img class="profile-user-img img-responsive img-circle" src="/public/assets/images/admin/user4-128x128.jpg" alt="User profile picture">
+                    <img class="profile-user-img img-responsive img-circle" src="http://www.socialagent.me/wp-content/uploads/2014/07/avatarDefault.png" alt="User profile picture">
 
                     <h3 class="profile-username text-center">{{ $company->name }}</h3>
 
@@ -45,30 +45,6 @@
             </div>
             <!-- /.box -->
 
-            <!-- About Me Box -->
-            <div class="box box-primary">
-                <div class="box-header with-border">
-                    <h3 class="box-title">About</h3>
-                </div>
-                <!-- /.box-header -->
-                <div class="box-body">
-                    <strong><i class="fa fa-phone margin-r-5"></i> Contact</strong>
-                    <p class="text-muted">{{ $company->contact }} </p>
-                    <p class="text-muted">{{ $company->contact_phone }} </p>
-
-                    <hr>
-
-                    <strong><i class="fa fa-map-marker margin-r-5"></i> Location</strong>
-                    <p class="text-muted">{{ $company->city }} {{ $company->state }}</p>
-
-                    <hr>
-
-                    <strong><i class="fa fa-file-text-o margin-r-5"></i> Notes</strong>
-                    <p>{{ $company->notes }}.</p>
-                </div>
-                <!-- /.box-body -->
-            </div>
-            <!-- /.box -->
         </div>
         <!-- /.col -->
         <div class="col-md-9">
@@ -91,10 +67,14 @@
                                     <div><strong>City:</strong> {{ $company->city }}</div>
                                     <div><strong>State:</strong> {{ $company->state }}</div>
                                     <div><strong>Zip:</strong> {{ $company->zip }}</div>
+                                    <br />
+                                    <br />
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="box">
+                                    <div><strong>Contact:</strong> {{ $company->contact }}</div>
+                                    <div><strong>Contact Phone:</strong> {{ $company->contact_phone }}</div>
                                     <div><strong>Phone:</strong> {{ $company->phone }}</div>
                                     <div><strong>Fax:</strong> {{ $company->fax }}</div>
                                     <div><strong>Web Site:</strong> {{ $company->website }}</div>
@@ -106,102 +86,80 @@
                             </div>
                         </div>
 
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="box">
+                                    <strong><i class="fa fa-file-text-o margin-r-5"></i> Notes</strong>
+                                    <p>{{ $company->notes }}.</p>
+                                </div>
+                            </div>
+                        </div>
+
                         <!-- /.company info -->
                     </div>
                     <!-- /.tab-pane -->
                     <div class="tab-pane" id="company-clients">
-                        <!-- The timeline -->
-                        <ul class="timeline timeline-inverse">
-                            <!-- timeline time label -->
-                            <li class="time-label">
-                        <span class="bg-red">
-                          10 Feb. 2014
-                        </span>
-                            </li>
-                            <!-- /.timeline-label -->
-                            <!-- timeline item -->
-                            <li>
-                                <i class="fa fa-envelope bg-blue"></i>
+                        <!-- company clients -->
 
-                                <div class="timeline-item">
-                                    <span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
-
-                                    <h3 class="timeline-header"><a href="#">Support Team</a> sent you an email</h3>
-
-                                    <div class="timeline-body">
-                                        Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles,
-                                        weebly ning heekya handango imeem plugg dopplr jibjab, movity
-                                        jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle
-                                        quora plaxo ideeli hulu weebly balihoo...
-                                    </div>
-                                    <div class="timeline-footer">
-                                        <a class="btn btn-primary btn-xs">Read more</a>
-                                        <a class="btn btn-danger btn-xs">Delete</a>
-                                    </div>
-                                </div>
-                            </li>
-                            <!-- END timeline item -->
-                            <!-- timeline item -->
-                            <li>
-                                <i class="fa fa-user bg-aqua"></i>
-
-                                <div class="timeline-item">
-                                    <span class="time"><i class="fa fa-clock-o"></i> 5 mins ago</span>
-
-                                    <h3 class="timeline-header no-border"><a href="#">Sarah Young</a> accepted your friend request
-                                    </h3>
-                                </div>
-                            </li>
-                            <!-- END timeline item -->
-                            <!-- timeline item -->
-                            <li>
-                                <i class="fa fa-comments bg-yellow"></i>
-
-                                <div class="timeline-item">
-                                    <span class="time"><i class="fa fa-clock-o"></i> 27 mins ago</span>
-
-                                    <h3 class="timeline-header"><a href="#">Jay White</a> commented on your post</h3>
-
-                                    <div class="timeline-body">
-                                        Take me to your leader!
-                                        Switzerland is small and neutral!
-                                        We are more like Germany, ambitious and misunderstood!
-                                    </div>
-                                    <div class="timeline-footer">
-                                        <a class="btn btn-warning btn-flat btn-xs">View comment</a>
+                        <!-- box -->
+                        <div class="box-body">
+                            <div id="company_clients_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <table id="company_clients" class="table table-bordered table-striped hover dataTable" role="grid"
+                                               aria-describedby="company_clients_info">
+                                            <thead>
+                                            <tr role="row">
+                                                <th class="sorting_asc" tabindex="0" aria-controls="company_clients" rowspan="1" colspan="1"
+                                                    aria-sort="ascending" aria-label="Type: activate to sort column descending"
+                                                    style="width: 80px;">Type
+                                                </th>
+                                                <th class="sorting" tabindex="0" aria-controls="company_clients" rowspan="1" colspan="1"
+                                                    aria-label="Name: activate to sort column ascending" style="width: 195px;">Name
+                                                </th>
+                                                <th class="sorting" tabindex="0" aria-controls="company_clients" rowspan="1" colspan="1"
+                                                    aria-label="Active: activate to sort column ascending" style="width: 30px;">Active
+                                                </th>
+                                                <th class="sorting" tabindex="0" aria-controls="company_clients" rowspan="1" colspan="1"
+                                                    aria-label="CSS grade: activate to sort column ascending" style="width: 50px;">
+                                                    Action
+                                                </th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            {{--@foreach($company_clients as $company)--}}
+                                                {{--<tr role="row" class="odd"  data-href="{{URL::to('/admin/company/' . $company->id)}}">--}}
+                                                    {{--<td class="sorting_1">{{ $company->type }}</td>--}}
+                                                    {{--<td>{{ $company->name }}</td>--}}
+                                                    {{--<td>{{ $company->notes }}</td>--}}
+                                                    {{--<td>{{ $company->active }}</td>--}}
+                                                    {{--<td>--}}
+                                                        {{--{{ link_to_action('Admin\CompanyController@show', $title = 'Show',--}}
+                                                            {{--$parameters = array($company->id),--}}
+                                                            {{--$attributes = array('class' => 'btn btn-xs btn-success')) }}--}}
+                                                        {{--{{ link_to_action('Admin\CompanyController@edit', $title = 'Edit',--}}
+                                                            {{--$parameters = array($company->id),--}}
+                                                            {{--$attributes = array('class' => 'btn btn-xs btn-info')) }}--}}
+                                                        {{--delete</td>--}}
+                                                {{--</tr>--}}
+                                            {{--@endforeach--}}
+                                            </tbody>
+                                            <tfoot>
+                                            <tr>
+                                                <th rowspan="1" colspan="1">Type</th>
+                                                <th rowspan="1" colspan="1">Name</th>
+                                                <th rowspan="1" colspan="1">Active</th>
+                                                <th rowspan="1" colspan="1">Action</th>
+                                            </tr>
+                                            </tfoot>
+                                        </table>
                                     </div>
                                 </div>
-                            </li>
-                            <!-- END timeline item -->
-                            <!-- timeline time label -->
-                            <li class="time-label">
-                        <span class="bg-green">
-                          3 Jan. 2014
-                        </span>
-                            </li>
-                            <!-- /.timeline-label -->
-                            <!-- timeline item -->
-                            <li>
-                                <i class="fa fa-camera bg-purple"></i>
+                            </div>
+                        </div>
+                        <!-- /.box-body -->
 
-                                <div class="timeline-item">
-                                    <span class="time"><i class="fa fa-clock-o"></i> 2 days ago</span>
-
-                                    <h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
-
-                                    <div class="timeline-body">
-                                        <img src="http://placehold.it/150x100" alt="..." class="margin">
-                                        <img src="http://placehold.it/150x100" alt="..." class="margin">
-                                        <img src="http://placehold.it/150x100" alt="..." class="margin">
-                                        <img src="http://placehold.it/150x100" alt="..." class="margin">
-                                    </div>
-                                </div>
-                            </li>
-                            <!-- END timeline item -->
-                            <li>
-                                <i class="fa fa-clock-o bg-gray"></i>
-                            </li>
-                        </ul>
+                        <!-- /.company clients -->
                     </div>
                     <!-- /.tab-pane -->
 
