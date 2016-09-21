d48d3f8e51040cc346eece62e65eabe3e0342f2c
Lester Mesa <lesterm@gmail.com>
2016-09-21 20:48:57 +0000

    fixed form component password bootstrap styling

--- resources/views/admin/user/form.blade.php
+++ resources/views/admin/user/form.blade.php
@@ -3,6 +3,7 @@
     {{ Form::bsSelect('fax_id', null,$faxes,'col-sm-2 control-label') }}
     {{ Form::bsText('name',null,[],'col-sm-2 control-label') }}
     {{ Form::bsEmail('email',null,[],'col-sm-2 control-label') }}
+    {{ Form::bsPassword('password','col-sm-2 control-label') }}
     {{ Form::bsText('note',null,[],'col-sm-2 control-label') }}
     {{ Form::bsSubmit('Submit', '','btn btn-info pull-right') }}
 </div>
