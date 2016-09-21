2304f524ba0b8707e714e16b8a053c342e65f63c
Lester Mesa <lesterm@gmail.com>
2016-09-21 20:36:21 +0000

    created delete modal

--- resources/assets/js/admin.js
+++ resources/assets/js/admin.js
@@ -1,4 +1,5 @@
 $(function () {
+
         var companies_table = $("#companies").DataTable();
         var company_clients_table = $("#company_clients").DataTable();
         var providers_table = $("#providers").DataTable();
@@ -10,28 +11,39 @@ $(function () {
         var faxes_users_table = $("#fax_users").DataTable();
         var users_table = $("#users").DataTable();
 
-        $('#companies tbody').on('click', 'tr', function () {
-                var data = companies_table.row( this ).data();
-                window.location.href = $(this).data('href');
-        } );
+        $('table[data-form="deleteForm"]').on('click', '.form-delete', function(e){
+                e.preventDefault();
+                var $form=$(this);
+                $('#confirm').modal({ backdrop: 'static', keyboard: false })
+                    .on('click', '#delete-btn', function(){
+                            $form.submit();
+                    });
+        });
 
-        $('#providers tbody').on('click', 'tr', function () {
-                var data = providers_table.row( this ).data();
-                window.location.href = $(this).data('href');
-        } );
+        // $('#companies tbody').on('click', 'tr', function () {
+        //         var data = companies_table.row( this ).data();
+        //         window.location.href = $(this).data('href');
+        // } );
+        //
+        // $('#providers tbody').on('click', 'tr', function () {
+        //         var data = providers_table.row( this ).data();
+        //         window.location.href = $(this).data('href');
+        // } );
+        //
+        // $('#clients tbody').on('click', 'tr', function () {
+        //         var data = clients_table.row( this ).data();
+        //         window.location.href = $(this).data('href');
+        // } );
+        //
+        // $('#faxes tbody').on('click', 'tr', function () {
+        //         var data = faxes_table.row( this ).data();
+        //         window.location.href = $(this).data('href');
+        // } );
+        //
+        // $('#users tbody').on('click', 'tr', function () {
+        //         var data = users_table.row( this ).data();
+        //         window.location.href = $(this).data('href');
+        // } );
 
-        $('#clients tbody').on('click', 'tr', function () {
-                var data = clients_table.row( this ).data();
-                window.location.href = $(this).data('href');
-        } );
 
-        $('#faxes tbody').on('click', 'tr', function () {
-                var data = faxes_table.row( this ).data();
-                window.location.href = $(this).data('href');
-        } );
-
-        $('#users tbody').on('click', 'tr', function () {
-                var data = users_table.row( this ).data();
-                window.location.href = $(this).data('href');
-        } );
 });
