2bc91d7f4a95d6ed953caa05929e62a000d19c4c
Lester Mesa <lesterm@gmail.com>
2016-09-23 11:49:59 +0000

    fixed bug on row click preventing opening modal

--- resources/assets/js/admin.js
+++ resources/assets/js/admin.js
@@ -11,8 +11,42 @@ $(function () {
         var faxes_users_table = $("#fax_users").DataTable();
         var users_table = $("#users").DataTable();
 
+
+        $('#companies tbody').on('click', 'tr', function (e) {
+                if (e.target.name === 'delete_modal') return;
+                var data = companies_table.row( this ).data();
+                window.location.href = $(this).data('href');
+
+
+        } );
+
+        $('#providers tbody').on('click', 'tr', function () {
+                if (e.target.name === 'delete_modal') return;
+                var data = providers_table.row( this ).data();
+                window.location.href = $(this).data('href');
+        } );
+
+        $('#clients tbody').on('click', 'tr', function () {
+                if (e.target.name === 'delete_modal') return;
+                var data = clients_table.row( this ).data();
+                window.location.href = $(this).data('href');
+        } );
+
+        $('#faxes tbody').on('click', 'tr', function () {
+                if (e.target.name === 'delete_modal') return;
+                var data = faxes_table.row( this ).data();
+                window.location.href = $(this).data('href');
+        } );
+
+        $('#users tbody').on('click', 'tr', function () {
+                if (e.target.name === 'delete_modal') return;
+                var data = users_table.row( this ).data();
+                window.location.href = $(this).data('href');
+        } );
+
         $('table[data-form="deleteForm"]').on('click', '.form-delete', function(e){
                 e.preventDefault();
+                // e.stopPropagation();
                 var $form=$(this);
                 $('#confirm').modal({ backdrop: 'static', keyboard: false })
                     .on('click', '#delete-btn', function(){
@@ -20,30 +54,6 @@ $(function () {
                     });
         });
 
-        // $('#companies tbody').on('click', 'tr', function () {
-        //         var data = companies_table.row( this ).data();
-        //         window.location.href = $(this).data('href');
-        // } );
-        //
-        // $('#providers tbody').on('click', 'tr', function () {
-        //         var data = providers_table.row( this ).data();
-        //         window.location.href = $(this).data('href');
-        // } );
-        //
-        // $('#clients tbody').on('click', 'tr', function () {
-        //         var data = clients_table.row( this ).data();
-        //         window.location.href = $(this).data('href');
-        // } );
-        //
-        // $('#faxes tbody').on('click', 'tr', function () {
-        //         var data = faxes_table.row( this ).data();
-        //         window.location.href = $(this).data('href');
-        // } );
-        //
-        // $('#users tbody').on('click', 'tr', function () {
-        //         var data = users_table.row( this ).data();
-        //         window.location.href = $(this).data('href');
-        // } );
 
 
 });
