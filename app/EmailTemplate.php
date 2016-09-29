51f9b59b70c3d77b9d706299fb0b714d705214a9
Lester Mesa <lesterm@gmail.com>
2016-09-29 15:09:10 +0000

    development updates

---
+++ app/EmailTemplate.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Askedio\SoftCascade\Traits\SoftCascadeTrait;
+
+class EmailTemplate extends Model
+{
+    use SoftDeletes;
+    use SoftCascadeTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    public $fillable = [
+        'company_id', 'provider_id', 'name','body','signature', 'note'
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * Get the company that owns the client
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function company() {
+        return $this->belongsTo('App\Company');
+    }
+}
