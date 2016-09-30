bf6f2bfff9f7deb844d1a316174e3793e135aacc
Lester Mesa <lesterm@gmail.com>
2016-09-30 03:49:18 +0000

    creating a provider creates admin user account

--- app/Provider.php
+++ app/Provider.php
@@ -20,6 +20,36 @@ class Provider extends Entity
      */
     protected $dates = ['deleted_at'];
 
+    public static function boot()
+    {
+        parent::boot();
+
+        /**
+         * Listen to the Provider created event.
+         * - once a Provider entity has been created also create the provider's admin user account
+         *
+         * @param  $provider
+         * @return void
+         */
+        static::created(function(Provider $provider)
+        {
+            if ($provider->contact_email) {
+                User::create([
+                    'entity_id' => $provider->id,
+                    'first_name' => $provider->contact_first_name,
+                    'last_name' => $provider->contact_last_name,
+                    'email' => $provider->contact_email,
+                    'password' => str_random(10),
+                    'remember_token' => str_random(10),
+                    'note' => 'Provider Administrator',
+                    'active' => 1
+                ]);
+            }
+
+            return true;
+        });
+    }
+
     /**
      * Get the provider config for the provider
      *
