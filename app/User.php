d4afbbb2ae715e04e09c6ff5039a53321fa81884
Lester Mesa <lesterm@gmail.com>
2016-09-30 01:09:48 +0000

    Client GUI/Controller updates for STI

--- app/User.php
+++ app/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'client_id', 'fax_id', 'first_name', 'last_name', 'email', 'password', 'note'
+        'entity_id', 'fax_id', 'first_name', 'last_name', 'email', 'password', 'note'
     ];
 
     /**
