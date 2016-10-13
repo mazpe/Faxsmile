f27725158f0111f5130954205dd3a7420a0f842f
Lester Mesa <lesterm@gmail.com>
2016-10-13 14:04:39 +0000

    #4 creating a new user

--- resources/views/admin/user/form.blade.php
+++ resources/views/admin/user/form.blade.php
@@ -1,21 +1,23 @@
 <div class="box-body">
     <!-- name, title, value, label attributes, input attributes -->
-    @if ( isset($user) && $user->entity->type == 'client' && Auth::user()->isCompanyAdmin() )
+    @if ( isset($user) && isset($user->entity) && $user->entity->type == 'client' && Auth::user()->isCompanyAdmin() )
     {{ Form::bsSelect('entity_id', 'Client',
-        isset($user) ? $user->entity->id : null, $clients, // selected
+        isset($user) && isset($user->entity) ? $user->entity->id : null, $clients, // selected
         ['class' => 'col-sm-2 control-label'], ['id' => 'user-clients', 'class' =>'form-control', 'placeholder' => 'Select one...']) }}
+    @elseif(isset($user) && Auth::user()->isClientAdmin())
+        {{ Form::bsHidden('entity_id', $user->entity_id) }}
     @endif
 
-    @if ( isset($user) && $user->entity->type == 'client' && (Auth::user()->isCompanyAdmin() || Auth::user()->isClientAdmin()) )
+    @if ( isset($user) && isset($user->entity) && $user->entity->type == 'client' && (Auth::user()->isCompanyAdmin() || Auth::user()->isClientAdmin()) )
     {{ Form::bsSelect('fax_id', 'Fax',
         isset($user) ? $user->fax_id : null, // selected
-        isset($user->client) ? $user->client->faxes->pluck('number', 'id') : $faxes, // options
+        isset($user) && isset($user->client) ? $user->client->faxes->pluck('number', 'id') : $faxes, // options
         ['class' => 'col-sm-2 control-label'], ['id' => 'user-faxes', 'class' =>'form-control', 'placeholder' => 'Select one...']) }}
     @endif
 
-    @if (isset($user) && $user->entity->type == 'company')
+    @if (isset($user) && isset($user->entity) && $user->entity->type == 'company')
         {{ Form::bsSelect('entity_id', 'Company',
-            isset($user) ? $user->entity->id : null, $companies, // selected
+            isset($user) && isset($user->entity)? $user->entity->id : null, $companies, // selected
             ['class' => 'col-sm-2 control-label'], ['id' => 'user-companies', 'class' =>'form-control', 'placeholder' => 'Select one...']) }}
     @endif
     {{ Form::bsText('first_name', null, null, ['class' => 'col-sm-2 control-label'], ['class' =>'form-control']) }}
