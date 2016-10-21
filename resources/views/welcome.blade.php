d33e529f72f05608dbe7a96f1da4f22c7d92fe69
MiamiJack <dewebdude@gmail.com>
2016-10-20 22:23:02 -0400

    Removed Laravel References from home blade and added faxit logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,19 +73,8 @@
                 </div>
             @endif
 
-            <div class="content">
+            <div class="content center">
                 <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+<img src="{{ asset('/assets/images/home/faxit-cloud.png') }}" width="422" height="229" alt=""/> </div>
     </body>
 </html>
